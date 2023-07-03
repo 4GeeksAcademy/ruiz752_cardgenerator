26336d387d17473d077c1f6db99851304661a566
Caro72 <106418089+Caro72@users.noreply.github.com>
2023-07-03 19:36:02 +0000

    finish

--- src/app.js
+++ src/app.js
@@ -6,6 +6,42 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const cardElement = document.querySelector(".card");
+  const suit = randomSuit();
+  cardElement.classList.add("card");
+  cardElement.classList.add(suit);
+
+  const upperCornerElement = document.querySelector(".upperCorner");
+  upperCornerElement.classList.add(suit);
+
+  const lowerCornerElement = document.querySelector(".lowerCorner");
+  lowerCornerElement.classList.add(suit);
+
+  document.querySelector(".cardNumber").innerHTML = randomNumber();
+};
+
+let randomSuit = () => {
+  const suits = ["heart", "diamond", "spade", "club"];
+  const suitIndex = Math.floor(Math.random() * suits.length);
+  return suits[suitIndex];
+};
+
+let randomNumber = () => {
+  const numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "K",
+    "Q"
+  ];
+  const numberIndex = Math.floor(Math.random() * numbers.length);
+  return numbers[numberIndex];
 };
